5ee06d1dda83d25eff0a2628b34902f3f0fac687
Tamilarasu E <tamilarasu77080@gmail.com>
2026-09-11 19:58:39 +0530

    added f string

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def odd_or_even():
     #oddor even
     user = int(input("enter the number:"))
     if user%2==0:
-        print("even number")
+        print(f"{user} is an even number")
     else:
-        print("odd number")
+        print(f"{user} is an odd number")
 odd_or_even()
